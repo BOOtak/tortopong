99b8663c6e1eba84df75c818d7e5479c8f4d3cb3
Kirill <kolbasisha@gmail.com>
2014-03-20 10:31:24 +0700

    Added bouncing ball.

--- main.lua
+++ main.lua
@@ -17,13 +17,17 @@ function love.load()
 
     objects.paddle = {}
     objects.paddle.body = love.physics.newBody(world, paddlex, paddley, "dynamic")
-    objects.paddle.shape = love.physics.newRectangleShape(img_width, img_height)
-    objects.paddle.fixture = love.physics.newFixture(objects.paddle.body, objects.paddle.shape)
+    objects.paddle.shape = love.physics.newRectangleShape(0, 0, img_width, img_height)
+    objects.paddle.fixture = love.physics.newFixture(objects.paddle.body, objects.paddle.shape, 1)
 
     objects.center = {}
     objects.center.body = love.physics.newBody(world, screen_center_x, screen_center_y)
-    objects.center.shape = love.physics.newCircleShape(screen_center_x, screen_center_y, 0)
-    objects.center.fixture = love.physics.newFixture(objects.center.body, objects.center.shape)
+
+    objects.ball = {}
+    objects.ball.body = love.physics.newBody(world, screen_center_x, screen_center_y, "dynamic")
+    objects.ball.shape = love.physics.newCircleShape(10)
+    objects.ball.fixture = love.physics.newFixture(objects.ball.body, objects.ball.shape, 1)
+    objects.ball.fixture:setRestitution(1)
 
     love.physics.newDistanceJoint(objects.paddle.body, objects.center.body, objects.paddle.body:getX(), objects.paddle.body:getY(), objects.center.body:getX(), objects.center.body:getY())
 end
@@ -33,6 +37,8 @@ function love.draw()
     love.graphics.circle("line", screen_center_x, screen_center_y, circle_radius, 100)
     love.graphics.setColor(255,255,255)
     love.graphics.draw(paddle, objects.paddle.body:getX(), objects.paddle.body:getY(), objects.paddle.body:getAngle(), 1, 1, img_width/2, img_height/2)
+    love.graphics.setColor(255, 0, 0)
+    love.graphics.circle("fill", objects.ball.body:getX(), objects.ball.body:getY(), objects.ball.shape:getRadius())
 end
 
 function love.update(dt)
@@ -50,5 +56,8 @@ function love.update(dt)
     end
     if love.keyboard.isDown("right") then
         objects.paddle.body:applyForce(force_x, force_y)
+    end
+    if love.keyboard.isDown("a") then
+        objects.ball.body:applyForce(0, 40)
     end
 end
